6a170bdcec143f4841d67c2be47d917554923803
mahm0ud-0 <mahmoudaborasheed2017@gmail.com>
2024-04-07 17:57:35 +0300

    fix: show header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-
+import trollFace from '../assets/troll-face.png'
 
 export default function Header() {
 
+    console.log(trollFace)
     return (
         <header>
-            <img src="src/assets/troll-face.png" alt="" />
+            <img src={trollFace} alt="" />
             <h2>Meme Generator</h2>
         </header>
     )
